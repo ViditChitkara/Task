fc5b94447eafe472350cb734c0756853cc04b5dc
ViditChitkara <chitkara.vidit@gmail.com>
2017-10-21 01:08:08 +0530

    fixed send_file error

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -1,6 +1,6 @@
 class TrackingController < ApplicationController
   def image
     Email.markRead(params[:id])
-    send_data("#{Rails.root}/assets/images/small.png",:type => 'image/png', :disposition  =>  'inline', :x_sendfile => true)
+    send_file("#{Rails.root}/public/images/small.png",:type => 'image/png', :disposition  =>  'inline', :x_sendfile => true)
   end
 end
